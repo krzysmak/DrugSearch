f24ebd130cac5f08b94e04d0a115ea31405b49be
Krzysztof Makuracki <krzysiek@debian-BULLSEYE-live-builder-AMD64>
2022-05-14 19:20:31 +0200

    Added sorting buttons

--- DrugSearch/Static/DrugSearch/js/fetch.js
+++
@@ -1,9 +0,0 @@
-const searchForm = document.getElementById("searchForm");
-
-function handleSubmit(postForm) {
-    searchForm.addEventListener("submit", e => {
-        console.log("searchForm.val")
-    })
-}
-
-handleSubmit(searchForm)
